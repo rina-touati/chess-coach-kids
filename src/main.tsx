c28212ae8a98e27215490cfa59aa64fd25cfeec4
Rina <rina@marketing66.com>
2026-09-10 12:56:54 +0300

    Redirect GitHub Pages traffic to Vercel app

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const productionUrl = 'https://chess-coach-kids-one.vercel.app/'
+
+if (window.location.hostname === 'rina-touati.github.io' && window.location.pathname.startsWith('/chess-coach-kids')) {
+  window.location.replace(productionUrl)
+} else {
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+}
